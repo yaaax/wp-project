bab6f3187e10d3ee14eab7eeda87ec780c12d85c
Onni Hakala <onni@koodimonni.fi>
2016-03-08 11:42:02 +0200

    Fixed install.sh script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Get script directory
-DIR=$(dirname $(readlink -f "$0"))
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Install composer dependencies
 composer install --working-dir=$DIR/../
