c20e0dca27ad55ffe9d2a5affffa394cc1087672
Onni Hakala <onni@keksi.io>
2016-06-13 00:31:14 +0300

    Renamed noindex-robots mu-plugin

--- web/app/mu-plugins/noindex-robots.php
+++ web/app/mu-plugins/noindex-robots.php
@@ -6,14 +6,20 @@
  * Version: 0.1
  */
 
-// Hides staging/development from Search engines
+
 add_filter( 'robots_txt', 'geniem_hide_non_production_robots', 10, 2 );
+
+/**
+ * Hides staging/development from Search engines
+ * @param $output - Output into robots.txt
+ * @param $public - 1 or 0 depending if blog public meta is turned on
+ */
 function geniem_hide_non_production_robots( $output, $public ) {
     // Don't do anything if blog is already hidden
-    if( '0' == $public ) { return $output; }
+    if ( '0' === $public ) { return $output; }
 
     // Hide all non production sites.
-    if( defined( 'WP_ENV' ) && WP_ENV != 'production' ) {
+    if ( defined( 'WP_ENV' ) && WP_ENV !== 'production' ) {
         $output = "User-agent: *\n";
         $output .= "Disallow: /\n";
     }
