6b6128680148c7bf3a5e59f252be1923754df419
Onni Hakala <onni@keksi.io>
2016-07-08 11:37:08 +0300

    Use whoops errors always in development

--- config/environments/development.php
+++ config/environments/development.php
@@ -13,3 +13,13 @@ define( 'SCRIPT_DEBUG', true );
  * We use onnimonni/signaler for https in local development
  */
 define( 'FORCE_SSL_ADMIN', true );
+
+/**
+ * Enable Whoops errors in all situations
+ * - This forces us to fix all warnings&errors
+ */
+if ( class_exists('\Whoops\Run') ) {
+    $whoops = new \Whoops\Run;
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+    $whoops->register();
+}
