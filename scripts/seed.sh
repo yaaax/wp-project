87c2868cb866b17a120cf9f11509d3c958918d3c
Onni Hakala <onni@keksi.io>
2016-10-27 12:32:36 +0300

    Refactor seed script

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -5,23 +5,15 @@
 ##
 
 # Set defaults
-
-# This is for local development where MYSQL_HOST is unknown beforehand
-if [ "$MYSQL_HOST" == "" ] && [ "$DB_PORT_3306_TCP_ADDR" != "" ]; then
-  MYSQL_HOST=$DB_PORT_3306_TCP_ADDR
-fi
-
-# Use default port if not defined
-if [ "$MYSQL_PORT" == "" ]; then
-  MYSQL_PORT="3306"
-fi
+export DB_HOST=${DB_PORT_3306_TCP_ADDR-$DB_HOST}
+export DB_PORT=${MYSQL_PORT-$DB_PORT}
 
 # Wait until mysql is open
-nc -z $MYSQL_HOST $MYSQL_PORT
+nc -z $DB_HOST $DB_PORT
 if [[ $? != 0 ]] ; then
-  echo "Waiting mysql to open in $MYSQL_HOST:$MYSQL_PORT..."
+  echo "Waiting mysql to open in $DB_HOST:$DB_PORT..."
   declare -i i
-  while ! nc -z $MYSQL_HOST $MYSQL_PORT; do
+  while ! nc -z $DB_HOST $DB_PORT; do
     if [ "$i" == "15" ]; then
       echo "Error: Mysql process timeout"
       exit 1
@@ -43,17 +35,4 @@ echo "Starting to import database seed..."
 ##
 # Add seed or necessary changes to database depending on env
 ##
-if [ "$WP_ENV" = "production" ] || [ "$WP_ENV" = "staging" ]; then
-
-    # Don't use seed data in production yet
-    echo "Production/Staging don't use seed data at the moment"
-
-elif [ "$WP_ENV" = "development" ] || [ "$WP_ENV" = "testing" ]; then
-
-    # Run migrations from phinx
-    phinx seed:run -q
-
-    # Index content in elasticpress
-    wp elasticpress index --setup
-
-fi
+phinx seed:run -q
