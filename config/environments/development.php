aeefdb05414f5d167c4d13a43e778658a01e0d65
Onni Hakala <onni@keksi.io>
2016-11-22 20:46:34 +0200

    Remove Whoops->register() from development.php since it's automatic anyway

--- config/environments/development.php
+++ config/environments/development.php
@@ -15,16 +15,6 @@ define( 'SCRIPT_DEBUG', true );
 define( 'FORCE_SSL_ADMIN', true );
 
 /**
- * Enable Whoops errors in all situations
- * - This forces us to fix all warnings&errors
- */
-if ( ! defined( 'WP_CLI' ) && class_exists( '\Whoops\Run' ) ) {
-    $whoops = new \Whoops\Run;
-    $whoops->pushHandler( new \Whoops\Handler\PrettyPageHandler );
-    $whoops->register();
-}
-
-/**
  * Use object cache so that we don't have parity problems with production
  * but only cache values for 1 second so that developers can be more productive
  */
